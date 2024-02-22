bf90762503b509f2891e3879314499ff7ff246bc
Abir <abirhammou2001@gmail.com>
2024-02-22 10:59:14 +0100

    upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { ContainerComponent } from './container/container.component';
+import { ProfileComponent } from './profile/profile.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SideBarComponent,
+    ContainerComponent,
+    ProfileComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
